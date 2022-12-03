7ac273f2455aaeb06c4c611ff6de76dd15cd5942
RasmusUK <rasmus.kritensen@live.dk>
2022-12-03 17:22:49 +0100

    2 done

--- 2/Program.java
+++ 2/Program.java
@@ -1,32 +1,47 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
+
+import javax.imageio.plugins.tiff.FaxTIFFTagSet;
 
 class Program {
     public static void main(String[] args){
         String line;
-        ArrayList<Integer> calories = new ArrayList<>();
-        int acc = 0;
+        HashMap<String, Integer> roundToScore = new HashMap<>();
+        roundToScore.put("A X", 4);
+        roundToScore.put("A Y", 8);
+        roundToScore.put("A Z", 3);
+        roundToScore.put("B X", 1);
+        roundToScore.put("B Y", 5);
+        roundToScore.put("B Z", 9);
+        roundToScore.put("C X", 7);
+        roundToScore.put("C Y", 2);
+        roundToScore.put("C Z", 6);
+
+        HashMap<String, String> strategy = new HashMap<>();
+        strategy.put("A X", "A Z");
+        strategy.put("A Y", "A X");
+        strategy.put("A Z", "A Y");
+        strategy.put("B X", "B X");
+        strategy.put("B Y", "B Y");
+        strategy.put("B Z", "B Z");
+        strategy.put("C X", "C Y");
+        strategy.put("C Y", "C Z");
+        strategy.put("C Z", "C X");
+
+        int part1 = 0;
+        int part2 = 0;
+
         try(BufferedReader reader = new BufferedReader(new FileReader(args[0])))
         {
             while ((line = reader.readLine()) != null) {
-                if(line.isEmpty()) 
-                {
-                    calories.add(acc);
-                    acc = 0;
-                }
-                else
-                {
-                    acc += Integer.parseInt(line);
-                }
+                part1 += roundToScore.get(line);
+                part2 += roundToScore.get(strategy.get(line));
             }
         }
         catch(Exception e){}
-        Collections.sort(calories);
-        int best = calories.get(calories.size()-1);
-        int top3 = calories.get(calories.size()-1) + calories.get(calories.size()-2) + calories.get(calories.size()-3);
-        System.out.println("Best: " + best);
-        System.out.println("Top 3 total: " + top3);
+
+        System.out.println("Part 1: " + part1);
+        System.out.println("Part 2: " + part2);
     }
 }
