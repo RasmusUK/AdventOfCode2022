a9f24de9799873ff647ec860291f13f668e6dc81
RasmusUK <rasmus.kritensen@live.dk>
2022-12-04 14:17:44 +0100

    Updated script

--- Run.ps1
+++ Run.ps1
@@ -3,18 +3,15 @@ Set-Location .\1\
 javac .\Program.java && java Program .\input.txt
 Set-Location ..
 
-Write-Output ""
-Write-Output "===Day 2==="
+Write-Output "`n===Day 2==="
 Set-Location .\2\
 javac .\Program.java && java Program .\input.txt
 Set-Location ..
 
-Write-Output ""
-Write-Output "===Day 3==="
+Write-Output "`n===Day 3==="
 Set-Location .\3\
 javac .\Program.java && java Program .\input.txt
 Set-Location ..
 
-Write-Output ""
-Write-Output "===Day 4==="
+Write-Output "`n===Day 4==="
 dotnet run --project .\4\Program.fsproj
