090196dc759da9aea4e329603b3ba60766a7de94
RasmusUK <rasmus.kritensen@live.dk>
2022-12-15 15:41:12 +0100

    15 done

--- 15/Program.cs
+++ 15/Program.cs
@@ -8,109 +8,65 @@ namespace Program
         static void Main(string[] args)
         {
             var input = Lib.Lib.ReadInputFile();
-            
-            var part1 = Part1(input);
+            var sensors = ParseInput(input);
+
+            var part1 = Part1(sensors);
             System.Console.WriteLine($"Part 1 = {part1}");
 
-            var part2 = Part2(input);
+            var part2 = Part2(sensors);
             System.Console.WriteLine($"Part 2 = {part2}");
         }       
 
-        static int Part1(List<string> input)
+        static int Part1(List<Sensor> sensors)
         {
-            var sensors = ParseInput(input);
-            var positions = new List<(int, int, int)>();
-            var positionsOccupied = new List<int>();
+            var y = 2000000;
 
-            var row = 2000000;
+            var minX = sensors.Min(s => s.SensorPos.x - s.Delta);
+            var maxX = sensors.Max(s => s.SensorPos.x + s.Delta);
 
-            foreach(var s in sensors)
+            var score = 0;
+
+            for(int x = minX; x <= maxX; x++)
             {
-                if(s.SensorPos.y == row) positionsOccupied.Add(s.SensorPos.x);
-                if(s.BeaconPos.y == row) positionsOccupied.Add(s.BeaconPos.x);
-                positions.AddRange(s.GetOccupiedPositionsAtRow(row));
+                var pos = (x,y);
+                if(sensors.Any(s => s.IsBeaconOrSensor(pos))) continue;
+                
+                foreach(var s in sensors)
+                {
+                    if(s.IsInside((pos)))
+                    {
+                        score++;
+                        break;
+                    }
+                }
             }
-            
-            var alreadyOccupied = positionsOccupied.Distinct().Count();
 
-            positionsOccupied.Clear();
-
-            foreach(var pos in positions)
-            {
-                positionsOccupied.AddRange(GetXPositions(pos));
-            }
-
-            positionsOccupied = positionsOccupied.Distinct().ToList();;
-
-            var count = positionsOccupied.Count - alreadyOccupied;
-
-            return count;
+            return score;
         }
 
-        static int Part2(List<string> input)
+        static long Part2(List<Sensor> sensors)
         {
             var max = 4000000;
-            var sensors = ParseInput(input);
-            var xPossible = Enumerable.Range(0, 20).ToList();
-            var yPossible = Enumerable.Range(0, 20).ToList();
-            foreach(var s in sensors)
-            {
-                s.GetOccupiedPositions();
-                xPossible = xPossible.Except(s.CannotX).ToList();;
-                yPossible = yPossible.Except(s.CannotY).ToList();;
-            }
-       
+            var min = 0;
 
-            //var positionsOccupiedMap = new Dictionary<int, HashSet<int>>();
-            //var fullRange = new HashSet<int>(Enumerable.Range(0, max));
-            //var puzzle = new int[max+1,max+1];
-//
-            //foreach(var s in sensors)
-            //{
-            //    foreach(var pos in s.GetOccupiedPositions().Where(e => e.xStart >= 0 && e.xEnd <= max))
-            //    {
-            //        foreach(var p in GetXPositions(pos))
-            //        {
-            //            puzzle[p,pos.y] = 1;
-            //        }
-            //    } 
-            //}
-            //int j = 0;
-            //foreach(var s in sensors)
-            //{    
-            //    System.Console.WriteLine(j);
-//
-            //    s.GetOccupiedPositions();
-            //    for(int i = 0; i <= max; i++)
-            //    {
-            //        positionsOccupiedMap[i].UnionWith(s.PositionsMap[i]);
-            //    }
-            //    j++;
-            //}
-//
-            //for(int row = 0; row <= max; row++)
-            //{
-            //    if(row % 10 == 0) System.Console.WriteLine(row);
-            //    
-            //    var result = fullRange.Except(positionsOccupiedMap[row]).ToList();
-//
-            //    if(result.Count != 0) 
-            //    {
-            //        return result[0] * 4000000 + row;
-            //    } 
-            //}         
+            for(int y = 0; y <= max; y++)
+            {
+                var xRanges = sensors.Select(s => (s.MinXAtY(min, y), s.MaxXAtY(max, y))).Where(s => s.Item1 <= s.Item2).ToList();
+                xRanges.Sort((s1, s2) => s1.Item1.CompareTo(s2.Item1));
+                var xMin = xRanges.Min(s => s.Item1);
+                var xMax = xRanges.Max(s => s.Item2);
+                if(xMin != 0) return xMin; 
+                else if(xMax != max) return xMax;
+
+                for(int i = 0; i < xRanges.Count; i++)
+                {
+                    if(xRanges[0].Item1 <= xRanges[i].Item1 && xRanges[0].Item2 >= xRanges[i].Item1)
+                        xRanges[0] = (xMin, Math.Max(xRanges[0].Item2, xRanges[i].Item2));
+                    else return (long) (xRanges[0].Item2 + 1) * 4000000 + y;
+                }
+            }
 
             return 0;
-        }
-
-        static List<int> GetXPositions((int xStart, int xEnd, int _) pos)
-        {
-            var positions = new List<int>();
-            for(int i = pos.xStart; i <= pos.xEnd; i++)
-            {
-                positions.Add(i);
-            }
-            return positions;
         }
 
         static List<Sensor> ParseInput(List<string> input)
@@ -141,67 +97,18 @@ namespace Program
         {
             SensorPos = sensorPos;
             BeaconPos = beaconPos;
-            OccupiedPos = new List<(int, int, int)>();
-            CannotX = new List<int>();
-            CannotY = new List<int>();
+            Delta = Math.Abs(sensorPos.x - beaconPos.x) + Math.Abs(sensorPos.y - beaconPos.y);
         }
 
         public (int x, int y) SensorPos { get; set; }
-        public (int x, int y) BeaconPos { get; set; }
-        public List<int> CannotX { get; set; }
-        public List<int> CannotY { get; set; }
+        public (int x, int y) BeaconPos { get; set; }    
+        public int Delta { get; set; }
 
-        List<(int xStart, int xEnd, int y)> OccupiedPos { get; set; }
-        public HashSet<int>[] PositionsMap {get; set;}
-
-        public List<(int xStart, int xEnd, int y)> GetOccupiedPositions()
-        {
-            if(OccupiedPos.Count != 0) return OccupiedPos;
-
-            var positions = new List<(int, int, int)>();
-            var distance = GetManhattenDistance(SensorPos, BeaconPos);
-            var start = SensorPos.y - distance;
-            var end = SensorPos.y + distance;
-            if(end <= 0 && start >= 20) CannotY.Remove(SensorPos.y);
-            PositionsMap = new HashSet<int>[end+1];
-            for(int y = start; y <= end; y++)
-            {
-                var distanceLeft = distance - (Math.Abs(SensorPos.y - y));
-
-                int xStart = SensorPos.x - distanceLeft;
-                int xEnd = SensorPos.x + distanceLeft;
-
-                if(xEnd <= 0 && xStart >= 20) CannotX.Remove(SensorPos.x);
-                positions.Add((xStart, xEnd, y));
-                //PositionsMap[y] = Enumerable.Range(xStart, xEnd).ToHashSet();
-            }
-
-            OccupiedPos = positions;
-
-            return OccupiedPos;
-        }
-
-        public List<(int xStart, int xEnd, int y)> GetOccupiedPositionsAtRow(int row)
-        {
-            if(OccupiedPos.Count == 0) GetOccupiedPositions();
-            return OccupiedPos.Where(e => e.y == row).ToList();
-        }
-
-        private HashSet<int> GetXPositions((int xStart, int xEnd) pos)
-        {
-            var positions = new HashSet<int>();
-            for(int i = pos.xStart; i <= pos.xEnd; i++)
-            {
-                positions.Add(i);
-            }
-            return positions;
-        }
-
-        private int GetManhattenDistance((int x, int y) pos1, (int x, int y) pos2)
-        {
-            var x = Math.Abs(pos1.x - pos2.x);
-            var y = Math.Abs(pos1.y - pos2.y);
-            return x + y;
-        }
+        private int MinXAtY(int y) => SensorPos.x - Delta + Math.Abs(SensorPos.y - y);
+        private int MaxXAtY(int y) => SensorPos.x + Delta - Math.Abs(SensorPos.y - y);
+        public bool IsInside((int x, int y) pos) => pos.x >= MinXAtY(pos.y) && pos.x <= MaxXAtY(pos.y);
+        public bool IsBeaconOrSensor((int x, int y) pos) => pos == SensorPos || pos == BeaconPos;
+        public int MinXAtY(int min, int y) => Math.Max(MinXAtY(y), min);
+        public int MaxXAtY(int max, int y) => Math.Min(MaxXAtY(y), max);
     }
 }
